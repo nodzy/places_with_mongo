749a72c1f5518e2460f6b7e61f6aeeaab3b43361
Malena <malenacorl@gmail.com>
2016-02-08 18:11:50 +0100

    Add photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'pp'
+	
 Photo.all.each { |photo| photo.destroy }
 Place.all.each { |place| place.destroy }
 Place.create_indexes
 Place.load_all(File.open('./db/places.json'))
-Dir.glob("./db/image*.jpg") {|f| photo=Photo.new; photo.contents=File.open(f,'rb'); photo.save}
-Photo.all.each {|photo| place_id=photo.find_nearest_place_id 1*1609.34; photo.place=place_id; photo.save}
+Dir.glob("./db/*.jpg") {|f| photo=Photo.new; photo.contents=File.open(f,'rb'); photo.save}
+Photo.all.each {|photo| place_id=photo.find_nearest_place_id 22000609; photo.place=place_id; photo.save}
 pp Place.all.reject {|pl| pl.photos.empty?}.map {|pl| pl.formatted_address}.sort
+	
 
 
 
